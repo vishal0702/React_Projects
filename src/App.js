08262abd1011e0dc962336f614624583d1675102
Vishal Srivastava <vishalsrivi123@gmail.com>
2023-08-25 12:36:48 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
 
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
